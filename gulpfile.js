deab86b69e57011139d06b3a6948ab0fc2c041c7
Mark Labrecque <mark@fuseinteractive.com>
2016-01-05 08:30:14 -0800

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+
+// Gulp Sass Task 
+gulp.task('sass', function() {
+  gulp.src('./scss/{,*/}*.{scss,sass}')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Create Gulp Default Task
+// ------------------------
+// Having watch within the task ensures that 'sass' has already ran before watching
+// 
+// This setup is slightly different from the one on the blog post at
+// http://www.zell-weekeat.com/gulp-libsass-with-susy/#comment-1910185635
+gulp.task('default', ['serve'], function () {
+  gulp.watch('./scss/{,*/}*.{scss,sass}', ['sass'])
+});
+
